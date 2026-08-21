bbe6cd04f17ab4a17df620188035ad48d62d0e93
alex <alex.podikoglou@gmail.com>
2026-08-22 01:22:19 +0300

    feat: creation of Object

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -13,3 +13,25 @@ pub enum Object {
     Blob(Blob),
     Tree(Tree),
 }
+
+impl Object {
+    pub fn blob(blob: Blob) -> Object {
+        Object::Blob(blob)
+    }
+
+    pub fn tree(tree: Tree) -> Object {
+        Object::Tree(tree)
+    }
+}
+
+impl From<Blob> for Object {
+    fn from(val: Blob) -> Self {
+        Object::blob(val)
+    }
+}
+
+impl From<Tree> for Object {
+    fn from(val: Tree) -> Self {
+        Object::tree(val)
+    }
+}

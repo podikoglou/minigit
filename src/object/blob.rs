fc09921860b6f3a32cf637b16243a8c7e9f6d191
alex <alex.podikoglou@gmail.com>
2026-08-22 01:41:10 +0300

    feat: Blob::new

--- src/object/blob.rs
+++ src/object/blob.rs
@@ -7,11 +7,18 @@ use crate::hash::{HashObject, ObjectHash};
 #[derive(Debug)]
 pub struct Blob(Vec<u8>);
 
+impl Blob {
+    pub fn new(bytes: Vec<u8>) -> Self {
+        Self(bytes)
+    }
+}
+
 impl Display for Blob {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "{}", self.hash())
     }
 }
+
 impl HashObject for Blob {
     fn hash(&self) -> ObjectHash {
         Sha1::digest(&self.0).into()

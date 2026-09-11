57938d51fe0b5e45c719adce7cbebad70d8307f9
alex <alex.podikoglou@gmail.com>
2026-09-11 16:20:19 +0300

    refactor: remove useless .into()

--- tests/buckets.rs
+++ tests/buckets.rs
@@ -5,7 +5,7 @@ mod common;
 #[test]
 fn test_buckets_iter() {
     let path = include_repo!("fixtures/repo-1.tar");
-    let repo = Repo::open(path.path().into()).unwrap();
+    let repo = Repo::open(path.path()).unwrap();
 
     // there is one commit which contains one file, thus there are three objects in this repo. in
     // repo-1 they happen to start with different prefixes, thus there are three buckets.

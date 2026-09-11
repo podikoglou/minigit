04509bbe01f6f48582beaeac42b330fc5da286a8
alex <alex.podikoglou@gmail.com>
2026-09-11 15:33:33 +0300

    docs: comment include_repo! macro

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,11 +1,15 @@
 #[macro_export]
 macro_rules! include_repo {
     ($path:literal) => {{
+        // create temp directory to unpack repo
         let dir = tempfile::tempdir().unwrap();
+
+        // tar bytes of the repo
         let bytes = include_bytes!($path);
         let vec: Vec<u8> = bytes.into();
         let cursor = std::io::Cursor::new(vec);
 
+        // open tar and unpack it into the dir
         let mut archive = tar::Archive::new(cursor);
         archive.unpack(&dir).unwrap();
 

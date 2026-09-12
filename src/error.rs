0298eeacd95b7d5dfbf331f4ed1641777c345254
alex <alex.podikoglou@gmail.com>
2026-09-13 01:16:59 +0300

    docs: typo

--- src/error.rs
+++ src/error.rs
@@ -20,7 +20,7 @@ pub enum MinigitError {
     #[error("Object not found")]
     ObjectNotFound,
 
-    // Ideally, we'd take advante of winnow's `ParseError`, but it contains a lifetime and we'd need
+    // Ideally, we'd take advantage of winnow's `ParseError`, but it contains a lifetime and we'd need
     // to make this type something like `MinigitError<'a>`, which means we'd need to specify the
     // lifetime every time we use it.
     #[error("Error parsing: {0}")]

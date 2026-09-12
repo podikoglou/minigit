aa59c919cce025375d3c3a77bb41fcb88074dae8
alex <alex.podikoglou@gmail.com>
2026-09-12 08:12:40 +0300

    test: rewrite bucket test

--- tests/buckets.rs
+++ tests/buckets.rs
@@ -3,11 +3,21 @@ use minigit::Repo;
 mod common;
 
 #[test]
-fn test_buckets_iter() {
+fn test_bucket_contains_expected_objects() {
     let path = include_repo!("fixtures/repo-1.tar");
-    let repo = Repo::open(path.path()).unwrap();
+    let repo = Repo::open(path.path()).expect("should be able to open repo");
 
-    // there is one commit which contains one file, thus there are three objects in this repo. in
-    // repo-1 they happen to start with different prefixes, thus there are three buckets.
-    assert!(repo.store.buckets().is_ok_and(|buckets| buckets.len() == 3));
+    let buckets = repo
+        .store
+        .buckets()
+        .expect("should be able to list buckets");
+
+    assert_eq!(buckets.len(), 3, "should have three buckets");
+
+    for prefix in [0xbe, 0x7b, 0xfa] {
+        assert!(
+            buckets.iter().find(|e| e.prefix == prefix.into()).is_some(),
+            "should have bucket with expected prefix"
+        );
+    }
 }

4b009b5bae2ed86fa8710a29b1537f6ac54e72b0
alex <alex.podikoglou@gmail.com>
2026-09-13 00:48:33 +0300

    refactor: switch `storage::object::loose` to `MinigitError`

--- src/storage/object/loose/mod.rs
+++ src/storage/object/loose/mod.rs
@@ -7,12 +7,12 @@ pub use parser::parse_object;
 
 use std::{fs, path::Path};
 
-use crate::object::Object;
+use crate::{MinigitError, object::Object};
 
 /// Reads and parses an [Object] from a [PathBuf].
 ///
 /// Objects are typically small enough, so this is not a streaming operation.
-pub fn read_object(path: impl AsRef<Path>) -> Result<Object, anyhow::Error> {
+pub fn read_object(path: impl AsRef<Path>) -> Result<Object, MinigitError> {
     let contents = fs::read(path)?;
 
     parse_object(&contents)

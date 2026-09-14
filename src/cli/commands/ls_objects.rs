af044fdbf22c76e01ecaf371ebbaa835399367f5
alex <alex.podikoglou@gmail.com>
2026-09-15 00:06:34 +0300

    feat: implement `ls-objects --pretty-2`

--- src/cli/commands/ls_objects.rs
+++ src/cli/commands/ls_objects.rs
@@ -14,11 +14,16 @@ use strum::IntoDiscriminant;
 pub struct LsObjectsCommand {
     buckets: Vec<HashPrefix>,
     pretty: bool,
+    pretty_2: bool,
 }
 
 impl LsObjectsCommand {
     pub fn run(self) -> Result<(), MinigitError> {
-        let LsObjectsCommand { buckets, pretty } = self;
+        let LsObjectsCommand {
+            buckets,
+            pretty,
+            pretty_2,
+        } = self;
         let repo = Repo::open(env::current_dir()?)?;
 
         // TODO: can we remove this box?
@@ -36,19 +41,44 @@ impl LsObjectsCommand {
                 )
             };
 
-        if !pretty {
-            // default output, just print object hashes
-            for object in objects {
-                println!("{}", object?.hash()?);
+        match (pretty, pretty_2) {
+            (false, false) => {
+                // default output, just print object hashes
+                for object in objects {
+                    println!("{}", object?.hash()?);
+                }
             }
-        } else {
-            // pretty output, print object types
-            for lazy_object in objects.filter_map(Result::ok) {
-                let object = lazy_object.into_object()?;
+            (true, _) => {
+                for lazy_object in objects.filter_map(Result::ok) {
+                    let object = lazy_object.into_object()?;
 
-                println!("{} {:?}", lazy_object.hash()?, object.discriminant());
+                    println!("{} {:?}", lazy_object.hash()?, object.discriminant());
+                }
             }
-        }
+            (_, true) => {
+                // pretty output, print object types
+                for lazy_object in objects.filter_map(Result::ok) {
+                    let object = lazy_object.into_object()?;
+                    let hash = lazy_object.hash()?;
+
+                    match object {
+                        minigit::object::Object::Blob(blob) => {
+                            println!("{}  blob    {} bytes", hash, blob.0.len())
+                        }
+                        minigit::object::Object::Tree(tree) => {
+                            println!("{}  tree    {} entries", hash, tree.entries.len())
+                        }
+                        minigit::object::Object::Commit(commit) => {
+                            println!(
+                                "{}  commit  {}",
+                                hash,
+                                commit.description.lines().next().unwrap_or_default()
+                            )
+                        }
+                    }
+                }
+            }
+        };
 
         Ok(())
     }

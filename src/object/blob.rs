413cb52243129d807b3063df53f851a1e3a42d30
Alex <alex.podikoglou@gmail.com>
2026-09-15 12:45:32 +0300

    faet: implement `WriteLoose` for `Blob`

--- src/object/blob.rs
+++ src/object/blob.rs
@@ -16,6 +16,7 @@ impl Blob {
 
 impl WriteLoose for Blob {
     fn write_loose<W: Write>(&self, writer: &mut W) -> Result<(), crate::MinigitError> {
-        todo!()
+        writer.write_all(&self.0)?;
+        Ok(())
     }
 }

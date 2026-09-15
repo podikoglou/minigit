c5ef1715329a87f55c8a2b918cabff41a6578f59
Alex <alex.podikoglou@gmail.com>
2026-09-15 13:23:55 +0300

    refactor: remove old code

--- src/object/mod.rs
+++ src/object/mod.rs
@@ -28,28 +28,6 @@ pub enum Object {
 }
 
 impl Object {
-    // /// Writes the header of the object, to a [`Write`].
-    // pub fn write_header<W: Write>(&self, mut writer: W) -> Result<(), std::io::Error> {
-    //     let (tag, size) = match self {
-    //         Object::Blob(blob) => ("blob", blob.0.len()),
-    //         Object::Tree(_) => todo!("write tree header"),
-    //         Object::Commit(_) => todo!("write commit header"),
-    //     };
-    //
-    //     write!(writer, "{} {}\0", tag, size)
-    // }
-    //
-    // /// Writes the uncompressed object, including its header, to a [`Write`].
-    // pub fn write<W: Write>(&self, mut writer: W) -> Result<(), std::io::Error> {
-    //     self.write_header(&mut writer)?;
-    //
-    //     match self {
-    //         Object::Blob(blob) => blob.write(writer),
-    //         Object::Tree(tree) => tree.write(writer),
-    //         Object::Commit(commit) => commit.write(writer),
-    //     }
-    // }
-
     /// Creates a SHA1 hash of the object.
     pub fn hash(&self) -> Result<ObjectHash, MinigitError> {
         let mut buf = Vec::new();

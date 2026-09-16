5ae38dcc5805306faa3400123808ab00d6fc00e6
alex <alex.podikoglou@gmail.com>
2026-09-16 12:21:31 +0300

    style: format

--- src/storage/object/loose/mod.rs
+++ src/storage/object/loose/mod.rs
@@ -16,7 +16,10 @@ pub trait WriteLoose {
 /// Reads and parses an [Object] from a [`Read`].
 ///
 /// Objects are typically small enough, so this is not a streaming operation.
-pub fn read_object(mut reader: impl BufRead, context: ParserContext) -> Result<Object, MinigitError> {
+pub fn read_object(
+    mut reader: impl BufRead,
+    context: ParserContext,
+) -> Result<Object, MinigitError> {
     let mut buf = Vec::new();
     reader.read_to_end(&mut buf)?;
 
@@ -26,7 +29,10 @@ pub fn read_object(mut reader: impl BufRead, context: ParserContext) -> Result<O
 /// Reads, decompresses and parses an [Object] from a [`Read`].
 ///
 /// Objects are typically small enough, so this is not a streaming operation.
-pub fn read_object_compressed(reader: impl BufRead, context: ParserContext) -> Result<Object, MinigitError> {
+pub fn read_object_compressed(
+    reader: impl BufRead,
+    context: ParserContext,
+) -> Result<Object, MinigitError> {
     let mut decoder = ZlibDecoder::new(reader);
 
     let mut buf = Vec::new();

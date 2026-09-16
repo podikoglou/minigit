03f4f5430b5f0a507d783c7f9d039fb0ad2a7a9b
alex <alex.podikoglou@gmail.com>
2026-09-16 22:56:38 +0300

    refactor: migrate `WriteLoose` to the new `Identity`

--- src/identity.rs
+++ src/identity.rs
@@ -2,7 +2,11 @@
 //!
 //! It offers newtypes such as [`Identity`], which is consisted of a [`Name`] and [`Email`].
 
+use std::io::Write;
+
 use nutype::nutype;
+
+use crate::storage::object::loose::WriteLoose;
 
 #[derive(Debug, PartialEq, Eq, Clone)]
 pub struct Identity {
@@ -16,16 +20,24 @@ impl Identity {
     }
 }
 
+impl WriteLoose for Identity {
+    fn write_loose<W: Write>(&self, writer: &mut W) -> Result<(), crate::MinigitError> {
+        write!(writer, "{} <{}>", self.name, self.email)?;
+
+        Ok(())
+    }
+}
+
 #[nutype(
     sanitize(trim),
     validate(not_empty),
-    derive(Debug, PartialEq, Eq, Clone)
+    derive(Debug, PartialEq, Eq, Clone, Display)
 )]
 pub struct Name(String);
 
 #[nutype(
     sanitize(trim),
     validate(not_empty),
-    derive(Debug, PartialEq, Eq, Clone)
+    derive(Debug, PartialEq, Eq, Clone, Display)
 )]
 pub struct Email(String);

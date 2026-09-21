5a47efaba470dd45ed181c6beed60542957db6f4
alex <alex.podikoglou@gmail.com>
2026-09-21 18:53:27 +0300

    test: add property tests for hashes

--- src/object/hash.rs
+++ src/object/hash.rs
@@ -170,6 +170,9 @@ impl Debug for HashPrefix {
 mod test {
     use std::path::PathBuf;
 
+    use hegel::Generator;
+    use hegel::TestCase;
+    use hegel::generators as gs;
     use winnow::Parser;
 
     use crate::object::hash::{HashPrefix, ObjectHash, parse_object_hash_str};
@@ -214,6 +217,36 @@ mod test {
         assert_eq!(format!("{}", HashPrefix(0x10)), "10".to_string());
     }
 
+    // TODO: deduplciate with the other implementation of this in the codebase
+    #[hegel::composite]
+    pub fn hash(tc: &TestCase) -> ObjectHash {
+        tc.draw(
+            gs::arrays(gs::integers())
+                .map(|bytes: [u8; 20]| ObjectHash::from(bytes))
+                .print_as_debug(),
+        )
+    }
+
+    #[hegel::composite]
+    pub fn hash_prefix(tc: &TestCase) -> HashPrefix {
+        tc.draw(hash().map(|h| h.prefix()).print_as_debug())
+    }
+
+    #[hegel::test]
+    fn hash_prefix_debug_eq_display(tc: TestCase) {
+        let prefix = tc.draw(hash_prefix().print_as_debug());
+
+        assert_eq!(format!("{:?}", prefix), format!("{}", prefix));
+    }
+
+    #[hegel::test]
+    fn hash_prefix_length_2(tc: TestCase) {
+        let prefix = tc.draw(hash_prefix().print_as_debug());
+
+        assert_eq!(format!("{}", prefix).len(), 2);
+        assert_eq!(format!("{:?}", prefix).len(), 2);
+    }
+
     #[test]
     fn hash_display() {
         assert_eq!(
@@ -243,5 +276,18 @@ mod test {
             ),
             "dbc74b2244f57ed7ddf6e5b53c17826bfbf9be51".to_string()
         );
+    }
+    #[hegel::test]
+    fn hash_debug_eq_display(tc: TestCase) {
+        let hash = tc.draw(hash().print_as_debug());
+
+        assert_eq!(format!("{:?}", hash), format!("{}", hash));
+    }
+
+    #[hegel::test]
+    fn hash_len_40(tc: TestCase) {
+        let hash = tc.draw(hash().print_as_debug());
+
+        assert_eq!(format!("{}", hash).len(), 40);
     }
 }

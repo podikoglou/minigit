22d50dd59c387b1ab799594e40ab44a850d4e3d5
alex <alex.podikoglou@gmail.com>
2026-09-23 18:21:33 +0300

    feat: make `diff.sh` tool support other diff tools

--- tests/fixtures/scripts/diff.sh
+++ tests/fixtures/scripts/diff.sh
@@ -3,8 +3,12 @@
 # Re-encodes an object and diffs it with the original (decompressed).
 # Useful for debugging both parsing and writing.
 #
+# Respects the `$DIFF` variable (uses `diff --text --color -y` by default). Some other options:
+# - vimdiff
+# - nvim -d
+#
 # Example:
 #   scripts/diff.sh ./objects/commit-4.obj
 #
 
-diff --color --text -y <(cat "$1" | pigz -zd) <(minigit parse-object -b "$1" | pigz -zd) 
+${DIFF:-diff --text --color -y} <(cat "$1" | pigz -zd) <(minigit parse-object -b "$1" | pigz -zd) 
